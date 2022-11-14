f7a99b479e398e44cc04994757a110325141fa86
psegarra <pausegarra.b@gmail.com>
2022-11-14 19:06:55 +0100

    added another layer

--- number_classifier_train.py
+++ number_classifier_train.py
@@ -7,9 +7,13 @@ data = keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
 print("Dataset downloaded")
 
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
 model = keras.Sequential()
-model.add(keras.layers.Flatten(input_shape=(28,28)))
-model.add(keras.layers.Dense(128, activation="relu"))
+model.add(keras.layers.Flatten(input_shape=(28,28,1)))
+model.add(keras.layers.Dense(64, activation="relu"))
+model.add(keras.layers.Dense(64, activation="relu"))
 model.add(keras.layers.Dense(10, activation="softmax"))
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
@@ -17,6 +21,8 @@ model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=
 print("Training model...")
 model.fit(train_images, train_labels, epochs=10, verbose=0)
 print("Training finished")
+
+model.evaluate(test_images, test_labels)
 
 print("Saving model...")
 model.save('models/number_classifier.h5')
